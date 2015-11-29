4c104c84cdffc907cbd393364b402d79243452c7
George Murray <murray.george.m@gmail.com>
2015-11-29 12:58:42 -0800

    Add quotes around filenames to support spaces

--- process_uploads.sh
+++ process_uploads.sh
@@ -8,19 +8,19 @@ WEBROOT=/home/ubuntu/web_uploads
 #WEBROOT=/home/george/src/docker-ubuntu-vnc-desktop/test-web
 for FILE in $FILES
 do
-	if [ -f $FILE ]; then
-		BASEFILE=`basename $FILE`
-		if echo $FILE | grep -e '.sendanywhere$' > /dev/null; then
+	if [ -f "$FILE" ]; then
+		BASEFILE=`basename "$FILE"`
+		if echo "$FILE" | grep -e '.sendanywhere$' > /dev/null; then
 			echo "skipping in-progress file: $FILE"
-		elif identify $FILE > /dev/null; then
+		elif identify "$FILE" > /dev/null; then
 			echo "converting image file for web: $FILE"
-			WIDTH=`identify -format '%w' $FILE`
+			WIDTH=`identify -format '%w' "$FILE"`
 			TRANSFORM="-strip"
 			if [[ `echo "$WIDTH > $TARGETWIDTH" | bc` -eq '1' ]]; then
 				FACTOR=`echo "($TARGETWIDTH * 100) / $WIDTH" | bc`
 				TRANSFORM="$TRANSFORM -resize $FACTOR%"
 			fi
-			case `identify -format '%[exif:Orientation]' $FILE` in
+			case `identify -format '%[exif:Orientation]' "$FILE"` in
 			1) TRANSFORM="$TRANSFORM";;
 			2) TRANSFORM="$TRANSFORM -flop";;
 			3) TRANSFORM="$TRANSFORM -rotate 180";;
@@ -31,10 +31,10 @@ do
 			8) TRANSFORM="$TRANSFORM -rotate 270";;
 			*) TRANSFORM="$TRANSFORM";;
 			esac
-			convert $FILE $TRANSFORM $WEBROOT/$BASEFILE
-			mv $FILE $ALLROOT/$BASEFILE
+			convert "$FILE" $TRANSFORM "$WEBROOT/$BASEFILE"
+			mv "$FILE" "$ALLROOT/$BASEFILE"
 		else
-			mv $FILE $ALLROOT/$BASEFILE
+			mv "$FILE" "$ALLROOT/$BASEFILE"
 		fi
 	else
 		echo "Ignoring non-regular-file $FILE"
